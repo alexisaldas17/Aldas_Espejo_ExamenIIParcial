3b60ab9028d8a72aaf49bae7c5ee2ddbf8ca95ad
Bryan <bespejo7032@uta.edu.ec>
2022-07-25 19:48:38 -0500

    Correcion Cedula

--- src/app/usuario/registro/registro.page.ts
+++ src/app/usuario/registro/registro.page.ts
@@ -65,7 +65,7 @@ export class RegistroPage implements OnInit {
           Validators.required,
           Validators.minLength(6),
           Validators.maxLength(10),
-          Validators.pattern('^(?=.*[a-z]{2})(?=.*[A-Z]{2})(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}$'),
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}$'),
         ],
       ],
       confirmPassword: [
@@ -193,34 +193,36 @@ export class RegistroPage implements OnInit {
 
   AEvalidarCedula(control: AbstractControl): ValidationErrors | null {
     const cedula = control.value;
-    if (cedula.length === 10) {
-      const digito_region = (cedula.substring(0, 2));
+    if (cedula.length == 10) {
+      let digito_region = (cedula.substring(0, 2));
       if (digito_region >= 1 && digito_region <= 24) {
-        const ultimo_digito = cedula.substring(9, 10);
-        const pares = parseFloat(cedula.substring(1, 2)) + parseFloat(cedula.substring(3, 4)) +
-                    parseFloat(cedula.substring(5, 6)) + parseFloat(cedula.substring(7, 8));
-        let numero1 = parseFloat(cedula.substring(0, 1));
-        numero1 = (numero1 * 2);
-        if (numero1 > 9) { numero1 = (numero1 - 9); }
-        let numero3 = parseFloat(cedula.substring(2, 3));
-        numero3 = (numero3 * 2);
-        if (numero3 > 9) { numero3 = (numero3 - 9); }
-        let numero5 = parseFloat(cedula.substring(4, 5));
-        numero5 = (numero5 * 2);
-        if (numero5 > 9) { numero5 = (numero5 - 9); }
-        let numero7 = parseFloat(cedula.substring(6, 7));
-        numero7 = (numero7 * 2);
-        if (numero7 > 9) { numero7 = (numero7 - 9); }
-        let numero9 = parseFloat(cedula.substring(8, 9));
-        numero9 = (numero9 * 2);
-        if (numero9 > 9) { numero9 = (numero9 - 9); }
-        const impares = numero1 + numero3 + numero5 + numero7 + numero9;
-        const suma_total = (pares + impares);
-        const primer_digito_suma = String(suma_total).substring(0, 1);
-        const decena = (parseFloat(primer_digito_suma) + 1) * 10;
-        let digito_validador = decena - suma_total;
-        if (digito_validador === 10) { digito_validador = 0; }
-        if (digito_validador === ultimo_digito) { return null; }
+        var ultimo_digito = cedula.substring(9, 10);
+        var pares = parseInt(cedula.substring(1, 2)) + parseInt(cedula.substring(3, 4)) + parseInt(cedula.substring(5, 6)) + parseInt(cedula.substring(7, 8));
+        var numero1 = parseInt(cedula.substring(0, 1));
+        var numero1 = (numero1 * 2);
+        if (numero1 > 9) { var numero1 = (numero1 - 9); }
+        var numero3 = parseInt(cedula.substring(2, 3));
+        var numero3 = (numero3 * 2);
+        if (numero3 > 9) { var numero3 = (numero3 - 9); }
+        var numero5 = parseInt(cedula.substring(4, 5));
+        var numero5 = (numero5 * 2);
+        if (numero5 > 9) { var numero5 = (numero5 - 9); }
+        var numero7 = parseInt(cedula.substring(6, 7));
+        var numero7 = (numero7 * 2);
+        if (numero7 > 9) { var numero7 = (numero7 - 9); }
+        var numero9 = parseInt(cedula.substring(8, 9));
+        var numero9 = (numero9 * 2);
+        if (numero9 > 9) { var numero9 = (numero9 - 9); }
+        var impares = numero1 + numero3 + numero5 + numero7 + numero9;
+        var suma_total = (pares + impares);
+        var primer_digito_suma = String(suma_total).substring(0, 1);
+        var decena = (parseInt(primer_digito_suma) + 1) * 10;
+        var digito_validador = decena - suma_total;
+        if (digito_validador == 10)
+          var digito_validador = 0;
+        if (digito_validador == ultimo_digito) {
+          return null
+        }
       }
     }
     return { validarCedula: true };
